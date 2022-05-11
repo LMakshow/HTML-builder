29fbdc23fab83e2a6e23fd5395b91f1db39f570c
Maksym Lytvyn <makshow.at@gmail.com>
2022-05-11 19:18:58 +0300

    feat: complete task 03 files in folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,20 @@
+const { readdir, stat } = require ('fs');
+const path = require('path');
+const { stdout } = process;
+
+const filePath = path.join(__dirname, '/secret-folder');
+stdout.write(`The list of files in ${filePath}:\n`);
+readdir(filePath, { withFileTypes: true }, (err, files) => {
+  if (err) throw err;
+  files.forEach((file) => {
+    if (file.isFile()) {
+      stat(path.join(filePath, file.name), (err, stats) => {
+        if (err) throw err;
+        const name = path.parse(file.name).name;
+        let ext = path.extname(file.name).slice(1);
+        if (ext === '') ext = '(no ext)';
+        stdout.write(`${name} - ${ext} - ${stats.size / 1000}kb\n`);
+      });
+    }
+  })
+});
