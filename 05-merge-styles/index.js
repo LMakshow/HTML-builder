5b1cca4a0a8ad2baa6a495fa1396fed87e54cff9
Maksym Lytvyn <makshow.at@gmail.com>
2022-05-13 14:34:53 +0300

    refactor: promisify task 05 merge styles

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,24 +1,29 @@
 const fs = require('fs');
+const fsPromises = require('fs/promises');
 const path = require('path');
 const { stdout } = process;
 
 const stylesPath = path.join(__dirname, '/styles');
 const bundleCssPath = path.join(__dirname, '/project-dist', 'bundle.css');
 
-fs.writeFile(bundleCssPath, '', (err) => {
-  if (err) throw err;
-});
-
-fs.readdir(stylesPath, { withFileTypes: true }, (err, files) => {
-  if (err) throw err;
-  files.forEach((file) => {
-    if (file.isFile() && (path.extname(file.name) === '.css')) {
-      const input = fs.createReadStream(path.join(stylesPath, file.name), 'utf-8');
-      const output = fs.createWriteStream(bundleCssPath, { flags: 'a'});
-      input.pipe(output);
-      stdout.write(`Merged file ${file.name}\n`);
-    }
-  }
-  );
-  stdout.write(`Completed CSS bundle at ${bundleCssPath}\n`);
-});
+try {
+  // Create new empty bundle
+  fsPromises.writeFile(bundleCssPath, '')
+    .then(async () => {
+      // Reading styles directory
+      const files = await fsPromises.readdir(stylesPath, { withFileTypes: true });
+      files.forEach((file) => {
+        // For each file: if file is CSS, piping it to the bundle
+        if (file.isFile() && (path.extname(file.name) === '.css')) {
+          const input = fs.createReadStream(path.join(stylesPath, file.name), 'utf-8');
+          const output = fs.createWriteStream(bundleCssPath, { flags: 'a' });
+          input.pipe(output);
+          stdout.write(`Merged file ${file.name}\n`);
+        }
+      });
+      stdout.write(`Completed CSS bundle at ${bundleCssPath}\n`);
+    });
+}
+catch (err) {
+  console.log(err);
+}
